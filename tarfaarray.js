a0cc28f906527c1d1d85341ca010f26d504b19a3
Lena <helena12grupocev@gmail.com>
2026-05-27 13:42:19 -0300

    classe
    array

---
+++ tarfaarray.js
@@ -0,0 +1,6 @@
+let linguagens = ["JavaScript", "Python", "Java"]
+
+console.log("Minhas linguagens de programação:", linguagens)
+console.log("Primeira linguagem:", linguagens[0])
+console.log("Segunda linguagem:", linguagens[1])
+console.log("Terceira linguagem:", linguagens[2])
